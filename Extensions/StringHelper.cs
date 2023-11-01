7a43d77ebe0a33256ca0342ee19bcffba10a7ea1
berkcinazz <berk.cinaz@gmail.com>
2023-11-01 16:13:09 +0300

    Add project files.

---
+++ Extensions/StringHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleChallenges.Extensions
+{
+    public class StringHelper
+    {
+        public void WriteToConsoleOfFizzBuzzResult(List<int> nums)
+        {
+            foreach (int i in nums)
+            {
+                string result = string.Empty;
+                bool isMultiplesOfThree = i % 3 == 0;
+                bool isMultiplesOfFive = i % 5 == 0;
+                if (isMultiplesOfThree) result = "Fizz";
+                if (isMultiplesOfFive) result += "Buzz";
+                if (!isMultiplesOfFive && !isMultiplesOfThree) result = i.ToString();
+                Console.WriteLine(result);
+            }
+        }
+
+        public string Sum(string val1, string val2)
+        {
+            int value1; int value2;
+            Int32.TryParse(val1, out value1);
+            Int32.TryParse(val2, out value2);
+            return (value1 + value2).ToString();
+        }
+        
+        public string Subtract(string val1, string val2)
+        {
+            int value1; int value2;
+            Int32.TryParse(val1, out value1);
+            Int32.TryParse(val2, out value2);
+            return (value1 - value2).ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using ExampleChallenges.Extensions;
+
+StringHelper stringHelper = new();
+
+#region FizzBuzz
+
+Console.WriteLine("FizzBuzz");
+var fizzBuzzNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15 };
+
+stringHelper.WriteToConsoleOfFizzBuzzResult(fizzBuzzNumbers);
+
+#endregion
+
+Console.WriteLine("-------------");
+
+#region String Sum
+
+Console.WriteLine("String Sum");
+var sumResult1 = stringHelper.Sum("1", "2");
+var sumResult2 = stringHelper.Sum("a", "2");
+var sumResult3 = stringHelper.Sum("a", "b");
+
+Console.WriteLine(sumResult1);//3
+Console.WriteLine(sumResult2);//2
+Console.WriteLine(sumResult3);//0
+
+#endregion
+
+Console.WriteLine("-------------");
+
+#region String Subtraction
+
+Console.WriteLine("String Subtraction");
+var subtractResult1 = stringHelper.Subtract("1", "2");
+var subtractResult2 = stringHelper.Subtract("5", "4");
+var subtractResult3 = stringHelper.Subtract("a", "2");
+var subtractResult4 = stringHelper.Subtract("a", "b");
+
+Console.WriteLine(subtractResult1);//-1
+Console.WriteLine(subtractResult2);//1
+Console.WriteLine(subtractResult3);//-2
+Console.WriteLine(subtractResult4);//0
+
+#endregion
